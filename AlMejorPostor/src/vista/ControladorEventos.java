2e464f9bef17987695fd5006b7b1c31633462122
Usuario <Usuario@DESKTOP-AL8VFSB>
2024-11-13 00:35:07 -0300

    Se borraron comentarios inadecuados

--- AlMejorPostor/src/vista/ControladorEventos.java
+++ AlMejorPostor/src/vista/ControladorEventos.java
@@ -4,7 +4,6 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 
 import logica.GestorOfertas;
